62d8096e4e6f51f1faf8c8f9e44f2333d2e486ff
Francisco Montalvao <f.montalvao@outlook.com>
2026-06-28 09:22:18 -0300

    chore: cleanup remaining file deletions and untracked files

--- src/main/java/com/francisco_montalvao/gestao_de_pedidos/dto/request/FiltroCategoriaRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.francisco_montalvao.gestao_de_pedidos.dto.request;
-
-import org.springframework.web.bind.annotation.BindParam;
-
-public record FiltroCategoriaRequestDTO(
-        @BindParam("categoria")
-        String nome
-) {
-}
---
+++ src/main/java/com/francisco_montalvao/gestao_de_pedidos/dto/response/CategoriaSimplesDTO.java
@@ -0,0 +1,7 @@
+package com.francisco_montalvao.gestao_de_pedidos.dto.response;
+
+public record CategoriaSimplesDTO(
+        Long id,
+        String nome
+) {
+}
